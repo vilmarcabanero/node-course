1b8158fb01aeb61a2fe411b47197f9bdf6b56153
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-05-22 23:59:51 +0800

    11. Importing npm Modules

--- notes-app/src/app.js
+++ notes-app/src/app.js
@@ -1,3 +1,9 @@
-import * as notes from './notes.js'
+import validator from 'validator'
+// import * as notes from './notes.js'
 
-console.log(notes.getNotes())
+// console.log(notes.getNotes())
+
+const email = 'vil@entropiya.net'
+const isEmail = validator.isEmail(email)
+const url = validator.isURL('entropiya.net')
+console.log(url)
